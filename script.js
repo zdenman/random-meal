1845dc32c4f08ade4895b000057fbe546388a9a4
zdenman <cemanzdeno@gmail.com>
2024-09-26 22:37:42 +0200

    dodanie ingrediencie, aj recepty object

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const meal = document.querySelector("#meal");
 const buttonSubmit = document.querySelector("#button-submit");
 const result = document.querySelector(".result");
 const mealList = document.querySelector(".meal-list");
+let recipes = [];
 
 let recepty = [
   "Lazanje",
@@ -36,16 +37,62 @@ recepty.forEach((recept) => {
   mealList.insertAdjacentHTML("beforeend", html);
 });
 
-buttonSubmit.addEventListener("click", (e) => {
-  e.preventDefault();
+
+// Adding recipe on click----------------- + Enter
+function addRecipe() {
+  console.log(meal.value);
+  // Adding recipe to the recipe object
+  recipes[meal.value.trim()] = {
+    ingredients: []
+  }
+  // Showing random result on the page from recepty array
   result.textContent = recepty[randomInt];
+  // Adding meal from input to the array
   recepty.push(meal.value);
+  // Clearing input field
+  mealList.innerHTML = "";
+}
+// Updating recipe list ---------------------------------------
+function updateRecipeList() {
+  // Clear the current list of recipes
   mealList.innerHTML = "";
 
+  
+  // Creating list of recipes-------------
   recepty.forEach((recept) => {
     //   console.log(recept);
     const html = `<li>${recept}</li>`;
     mealList.insertAdjacentHTML("beforeend", html);
   });
-  console.log(recepty);
+  const mealListItems = document.querySelectorAll(".meal-list li");
+  // Adding ingredients for each recipe on click----------------- rozkliknutia +++
+  const ingredients = "cestoviny, meso, zelenina, syr, mlieko, mrkva, cukor, olej, slanina, syr, korenie";
+ mealListItems.forEach((item) => {
+  // console.log(item)
+   item.addEventListener("click", () => {
+     item.insertAdjacentHTML("beforeend", `<p>${ingredients}</p>`);
+   });
+ });
+}
+
+//  Adding recipe on click----------------- + Button
+buttonSubmit.addEventListener("click", (e) => {
+  e.preventDefault();
+  addRecipe();
+  updateRecipeList();
+
+
+  // Adding ingredients for each recipe on click-----------------
+//  const mealListItems = document.querySelectorAll(".meal-list li");
+//  console.log(mealListItems);
+//  const ingredients = "cestoviny, meso, zelenina, syr, mlieko, mrkva, cukor, olej, slanina, syr, korenie";
+//  mealListItems.forEach((item) => {
+//   console.log(item)
+//    item.addEventListener("click", () => {
+//      item.insertAdjacentHTML("beforeend", `<p>${ingredients}</p>`);
+//    });
+//  });
+ console.log(recipes);
+ console.log(recipes[0]);
+ 
 });
