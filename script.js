489c9676ac0786061709fc4c6580fbe698c53e69
zdenman <cemanzdeno@gmail.com>
2024-09-27 14:55:30 +0200

    object, ingredients, id
    createbd starting object and object structure, added functionality to add ingredients for recipes. Each recipe have own id and source link.

--- script.js
+++ script.js
@@ -2,7 +2,21 @@ const meal = document.querySelector("#meal");
 const buttonSubmit = document.querySelector("#button-submit");
 const result = document.querySelector(".result");
 const mealList = document.querySelector(".meal-list");
-let recipes = [];
+let genJedlo = document.querySelector("#gen-jedlo");
+let recipes = [
+  {
+    id: 1727425558221,
+    title: "Lazanje",
+    ingredients: ["kory", "pretlak", "mletvo meso"],
+    link: "",
+  },
+  {
+    id: 1727426707989,
+    title: "Vajcia",
+    ingredients: ["vajcia", "olej"],
+    link: "",
+  },
+];
 
 let recepty = [
   "Lazanje",
@@ -20,61 +34,87 @@ let recepty = [
   "Kuracie nugetky",
   "Ryzovie rezance s kuracim mesom",
 ];
-function getRandomIntInclusive(min, max) {
-  min = Math.ceil(min); // Round up to the nearest integer
-  max = Math.floor(max); // Round down to the nearest integer
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
 
-// Example usage:
-const randomInt = getRandomIntInclusive(0, recepty.length);
-// console.log(recepty[randomInt]);
-
-// List of meal
+// List of meal from array
 recepty.forEach((recept) => {
   //   console.log(recept);
   const html = `<li>${recept}</li>`;
   mealList.insertAdjacentHTML("beforeend", html);
 });
 
+function showRandomRecipe() {
+  function getRandomIntInclusive(min, max) {
+    min = Math.ceil(min); // Round up to the nearest integer
+    max = Math.floor(max); // Round down to the nearest integer
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+  // Example usage:
+  const randomInt = getRandomIntInclusive(0, recepty.length);
+  // Showing random result on the page from recepty array
+  result.textContent = recepty[randomInt];
+}
 
 // Adding recipe on click----------------- + Enter
 function addRecipe() {
   // console.log(meal.value);
   // Adding recipe to the recipe object
   const newRecipe = {
+    id: Date.now(),
     title: meal.value.trim(),
     ingredients: [],
-  }
+    link: "",
+  };
   recipes.push(newRecipe);
   // Showing random result on the page from recepty array
-  result.textContent = recipes[randomInt];
+  //   result.textContent = recipes[randomInt];
   // Adding meal from input to the array
   recepty.push(meal.value);
   // Clearing input field
   mealList.innerHTML = "";
 }
+
 // Updating recipe list ---------------------------------------
 function updateRecipeList() {
   // Clear the current list of recipes
   mealList.innerHTML = "";
 
-  
   // Creating list of recipes-------------
-  recepty.forEach((recept) => {
+  recipes.forEach((recipe) => {
     //   console.log(recept);
-    const html = `<li>${recept}</li>`;
-    mealList.insertAdjacentHTML("beforeend", html);
+    // const html = `<li>${recipe.title}</li>
+    // <p class="ingredients-container">${[...recipe.ingredients]}</p></br>`;
+    // mealList.insertAdjacentHTML("beforeend", html);
+    const mealListSingleItem = document.createElement("li");
+    mealListSingleItem.textContent = recipe.title;
+
+    const addIngredientButton = document.createElement("button");
+    addIngredientButton.textContent = "+";
+    addIngredientButton.addEventListener("click", () => {
+      addIngredient(recipe.id);
+    });
+    mealListSingleItem.appendChild(addIngredientButton);
+    mealList.appendChild(mealListSingleItem);
   });
-  const mealListItems = document.querySelectorAll(".meal-list li");
-  // Adding ingredients for each recipe on click----------------- rozkliknutia +++
-  const ingredients = "cestoviny, meso, zelenina, syr, mlieko, mrkva, cukor, olej, slanina, syr, korenie";
- mealListItems.forEach((item) => {
-  // console.log(item)
-   item.addEventListener("click", () => {
-     item.insertAdjacentHTML("beforeend", `<p>${ingredients}</p>`);
-   });
- });
+  //   const mealListItems = document.querySelectorAll(".meal-list li");
+
+  //   mealListItems.forEach((mealItem) => {
+  //     mealItem.addEventListener("click", () => {
+  //       let ingContainer = document.querySelector(".ingredients-container");
+  //       ingContainer.classList.toggle("show");
+  //       console.log("clicked on ingredients");
+  //     });
+  //   });
+}
+
+function addIngredient(recipeId) {
+  const ingredient = prompt("enter ingredients:");
+  if (ingredient) {
+    const recipe = recipes.find((r) => r.id === recipeId);
+    if (recipe) {
+      recipe.ingredients.push(ingredient);
+      console.log(recipes);
+    }
+  }
 }
 
 //  Adding recipe on click----------------- + Button
@@ -82,19 +122,24 @@ buttonSubmit.addEventListener("click", (e) => {
   e.preventDefault();
   addRecipe();
   updateRecipeList();
-
-
-  // Adding ingredients for each recipe on click-----------------
-//  const mealListItems = document.querySelectorAll(".meal-list li");
-//  console.log(mealListItems);
-//  const ingredients = "cestoviny, meso, zelenina, syr, mlieko, mrkva, cukor, olej, slanina, syr, korenie";
-//  mealListItems.forEach((item) => {
-//   console.log(item)
-//    item.addEventListener("click", () => {
-//      item.insertAdjacentHTML("beforeend", `<p>${ingredients}</p>`);
-//    });
-//  });
- console.log(recipes);
- console.log(recipes[0]);
- 
+  console.log(recipes);
 });
+
+// Show random recipe on button click
+genJedlo.addEventListener("click", (e) => {
+  console.log("click");
+  showRandomRecipe();
+});
+
+meal.addEventListener("focus", (e) => {
+  meal.value = "";
+});
+
+// const mealListItems = document.querySelectorAll(".meal-list li");
+
+// mealListItems.forEach((mealItem) => {
+//   mealItem.addEventListener("click", () => {});
+//   let ingContainer = document.querySelector(".ingredients-container");
+//   ingContainer.classList.toggle(".show");
+//   console.log("clicked on ingredients");
+// });
